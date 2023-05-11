e396203023b98c4e681719e8540629cd596904f4
AnnaFutryk <anna.futryk@gmail.com>
2023-05-11 17:55:07 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const refs = {
+    galleryList: document.querySelector('.gallery'),
+}
 
-console.log(galleryItems);
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+
+refs.galleryList.innerHTML = listItemsMarkup;
+
+const modal = new SimpleLightbox('.gallery__link', {
+    sourceAttr: 'href',
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function createListItemsMarkup(items) {
+    return items.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`).join('');
+};
